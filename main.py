1a098226da21e4c27d1393cb3576ce260fb720aa
Shahid <shahid451998@gmail.com>
2023-08-02 10:51:38 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+
+app = Flask(__name__)
+slack_event_api = SlackEventAdapter('e435a3403c117be73a76c7588aa5aaaa' , '/slack/events', app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
