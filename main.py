7e676f2d9c3b0a78f1d7b4b8f9c3c3463798a64a
Shahid <shahid451998@gmail.com>
2023-08-02 11:08:46 +0530

    jg

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 slack_event_api = SlackEventAdapter('e435a3403c117be73a76c7588aa5aaaa' , '/slack/events', app)
-
+#k
 if __name__ == "__main__":
     app.run(debug=True)
